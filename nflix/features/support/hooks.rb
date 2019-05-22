1e3eedd35f1d0fbf05a36bdc023b4b634bc860cc
Tiago Pereira da Cruz <tiago.cruz@prodeb.local>
2019-05-22 17:35:36 -0300

    Finalizando automação web ninja

--- nflix/features/support/hooks.rb
+++ nflix/features/support/hooks.rb
@@ -2,9 +2,13 @@ Before do
   @login_page = LoginPage.new
   @movie_page = MoviePage.new
   @sidebar = SideBarView.new
+
+  # page.driver.browser.manage.window.maximize
+  page.current_window.resize_to(1140, 900)
 end
 
-Before ('@login') do
+Before ("@login") do
+  user = CONFIG['users']['cat_manager']
   @login_page.go
-  @login_page.with('tony@stark.com', 'pwd456')
+  @login_page.with(user['email'], user['pass'])
 end
