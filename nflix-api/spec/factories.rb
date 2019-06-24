f2b9b65faebf0ce7a888e150c7f14cb93109ff22
Tiago Pereira <tiago.peng@gmail.com>
2019-06-24 19:20:17 -0300

    FactoryBot para melhor desempenho dos testes

---
+++ nflix-api/spec/factories.rb
@@ -0,0 +1,14 @@
+require_relative "models/user_model"
+
+FactoryBot.define do
+  factory :user, class: UserModel do
+    full_name { "Tiago Pereira" }
+    email { "tiagopereira1988@hotmail.com" }
+    password { "jarvis123" }
+
+    after(:build) do |user|
+      # puts "cleaning test data by email #{user.email}"
+      Database.new.delete_user(user.email)
+    end
+  end
+end
--- nflix-api/spec/post_spec.rb
+++ nflix-api/spec/post_spec.rb
@@ -1,8 +1,7 @@
 describe "post" do
   context "when new user" do
     before do
-      @new_user = { full_name: "Tiago Pereira", email: "tiagopereira1989@hotmail.com", password: "jarvis123" }
-      Database.new.delete_user(@new_user[:email])
+      @new_user = build(:user).to_hash
 
       @result = HTTParty.post(
         "http://192.168.99.100:3001/user",
@@ -16,6 +15,25 @@ describe "post" do
     it { expect(@result.response.code).to eql "200" }
   end
 end
+
+# describe "post" do
+#   context "when new user" do
+#     before do
+#       @new_user = { full_name: "Tiago Pereira", email: "tiagopereira1989@hotmail.com", password: "jarvis123" }
+#       Database.new.delete_user(@new_user[:email])
+
+#       @result = HTTParty.post(
+#         "http://192.168.99.100:3001/user",
+#         body: @new_user.to_json,
+#         headers: {
+#           "Content-Type" => "application/json",
+#         },
+#       )
+#     end
+
+#     it { expect(@result.response.code).to eql "200" }
+#   end
+# end
 
 # describe "cadastro" do
 #   it "novo usuario" do
