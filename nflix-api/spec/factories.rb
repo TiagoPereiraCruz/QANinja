905127d82edeef122ebfd04c3aa4f6982652d304
Tiago Pereira <tiago.peng@gmail.com>
2019-06-24 20:37:22 -0300

    Encapsulando verbo HTTP

--- nflix-api/spec/factories.rb
+++ nflix-api/spec/factories.rb
@@ -11,4 +11,10 @@ FactoryBot.define do
       Database.new.delete_user(user.email)
     end
   end
+
+  factory :user_wrong_email, class: UserModel do
+    full_name { "Tiago Pereira" }
+    email { "tiagopereira1988#hotmail.com" }
+    password { "jarvis123" }
+  end
 end
--- nflix-api/spec/post_spec.rb
+++ nflix-api/spec/post_spec.rb
@@ -2,19 +2,54 @@ describe "post" do
   context "when new user" do
     before do
       @new_user = build(:user).to_hash
-
-      @result = HTTParty.post(
-        "http://192.168.99.100:3001/user",
-        body: @new_user.to_json,
-        headers: {
-          "Content-Type" => "application/json",
-        },
-      )
+      @result = ApiUser.save(@new_user)
     end
-
     it { expect(@result.response.code).to eql "200" }
   end
-end
+
+  context "when wrong email" do
+    before do
+      @new_user = build(:user_wrong_email).to_hash
+      @result = ApiUser.save(@new_user)
+    end
+    it { expect(@result.response.code).to eql "412" }
+    it { expect(@result.parsed_response["msg"]).to eql "Oops. You entered a wrong email." }
+  end
+end #
+
+# describe "post" do
+#   context "when new user" do
+#     before do
+#       @new_user = build(:user).to_hash
+
+#       @result = HTTParty.post(
+#         "http://192.168.99.100:3001/user",
+#         body: @new_user.to_json,
+#         headers: {
+#           "Content-Type" => "application/json",
+#         },
+#       )
+#     end
+
+#     it { expect(@result.response.code).to eql "200" }
+#   end
+
+#   context "when wrong email" do
+#     before do
+#       @new_user = build(:user_wrong_email).to_hash
+
+#       @result = HTTParty.post(
+#         "http://192.168.99.100:3001/user",
+#         body: @new_user.to_json,
+#         headers: {
+#           "Content-Type" => "application/json",
+#         },
+#       )
+#     end
+#     it { expect(@result.response.code).to eql "412" }
+#     it { expect(@result.parsed_response["msg"]).to eql "Oops. You entered a wrong email." }
+#   end
+# end
 
 # describe "post" do
 #   context "when new user" do
