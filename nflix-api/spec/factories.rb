b7a24bbfe2ca573e5147aaac6df9e7d9251400ff
Tiago Pereira <tiago.peng@gmail.com>
2019-07-01 14:27:45 -0300

    Avançando com POST

--- nflix-api/spec/factories.rb
+++ nflix-api/spec/factories.rb
@@ -12,9 +12,54 @@ FactoryBot.define do
     end
   end
 
+  factory :regitered_user, class: UserModel do
+    full_name { "Tiago Pereira" }
+    email { "tiagopereira1988@hotmail.com" }
+    password { "jarvis123" }
+
+    after(:build) do |user|
+      # puts "cleaning test data by email #{user.email}"
+      Database.new.delete_user(user.email)
+      ApiUser.save(user.to_hash)
+    end
+  end
+
   factory :user_wrong_email, class: UserModel do
     full_name { "Tiago Pereira" }
     email { "tiagopereira1988#hotmail.com" }
     password { "jarvis123" }
+  end
+
+  factory :empty_name_user, class: UserModel do
+    full_name { "" }
+    email { "tiagopereira1988@hotmail.com" }
+    password { "jarvis123" }
+  end
+
+  factory :empty_email_user, class: UserModel do
+    full_name { "Tiago Pereira" }
+    email { "" }
+    password { "jarvis123" }
+  end
+
+  factory :empty_password_user, class: UserModel do
+    full_name { "Tiago Pereira" }
+    email { "tiagopereira1988@hotmail.com" }
+    password { "" }
+  end
+
+  factory :null_name_user, class: UserModel do
+    email { "tiagopereira1988@hotmail.com" }
+    password { "jarvis123" }
+  end
+
+  factory :null_email_user, class: UserModel do
+    full_name { "Tiago Pereira" }
+    password { "jarvis123" }
+  end
+
+  factory :null_password_user, class: UserModel do
+    full_name { "Tiago Pereira" }
+    email { "tiagopereira1988@hotmail.com" }
   end
 end
--- nflix-api/spec/post_spec.rb
+++ nflix-api/spec/post_spec.rb
@@ -7,6 +7,16 @@ describe "post" do
     it { expect(@result.response.code).to eql "200" }
   end
 
+  context "when duplicate email" do
+    before do
+      @new_user = build(:regitered_user).to_hash
+      @result = ApiUser.save(@new_user)
+    end
+
+    it { expect(@result.response.code).to eql "409" }
+    it { expect(@result.parsed_response["msg"]).to eql "Oops. Looks like you already have an account with this email address." }
+  end
+
   context "when wrong email" do
     before do
       @new_user = build(:user_wrong_email).to_hash
@@ -15,7 +25,61 @@ describe "post" do
     it { expect(@result.response.code).to eql "412" }
     it { expect(@result.parsed_response["msg"]).to eql "Oops. You entered a wrong email." }
   end
-end #
+
+  context "when empty name" do
+    before do
+      @new_user = build(:empty_name_user).to_hash
+      @result = ApiUser.save(@new_user)
+    end
+    it { expect(@result.response.code).to eql "412" }
+    it { expect(@result.parsed_response["msg"]).to eql "Validation notEmpty on full_name failed" }
+  end
+
+  context "when empty email" do
+    before do
+      @new_user = build(:empty_email_user).to_hash
+      @result = ApiUser.save(@new_user)
+    end
+    it { expect(@result.response.code).to eql "412" }
+    it { expect(@result.parsed_response["msg"]).to eql "Validation notEmpty on email failed" }
+  end
+
+  context "when empty password" do
+    before do
+      @new_user = build(:empty_password_user).to_hash
+      @result = ApiUser.save(@new_user)
+    end
+    it { expect(@result.response.code).to eql "412" }
+    it { expect(@result.parsed_response["msg"]).to eql "Validation notEmpty on password failed" }
+  end
+
+  context "When name is nulll" do
+    before do
+      @new_user = build(:null_name_user).to_hash
+      @result = ApiUser.save(@new_user)
+    end
+    it { expect(@result.response.code).to eql "412" }
+    it { expect(@result.parsed_response["msg"]).to eql "Users.full_name cannot be null" }
+  end
+
+  context "When email is nulll" do
+    before do
+      @new_user = build(:null_email_user).to_hash
+      @result = ApiUser.save(@new_user)
+    end
+    it { expect(@result.response.code).to eql "412" }
+    it { expect(@result.parsed_response["msg"]).to eql "Users.email cannot be null" }
+  end
+
+  context "When password is nulll" do
+    before do
+      @new_user = build(:null_password_user).to_hash
+      @result = ApiUser.save(@new_user)
+    end
+    it { expect(@result.response.code).to eql "412" }
+    it { expect(@result.parsed_response["msg"]).to eql "Users.password cannot be null" }
+  end
+end
 
 # describe "post" do
 #   context "when new user" do
