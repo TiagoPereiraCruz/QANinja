de42fbbb9e7cf99c79807dddeb335ec1f49a595f
Tiago Pereira da Cruz <tiago.cruz@prodeb.local>
2019-07-08 16:07:45 -0300

    adicionando estudo de Javascript

---
+++ js4t/variaveis.js
@@ -0,0 +1,16 @@
+// tipagem dinâmica, ou seja, atribui o tipo quando recebe o valor
+var number = 10
+console.log(number)
+
+// typeof - responsável por trazer o tipo da variável
+console.log(typeof number)
+
+var name = "Tiago"
+console.log(typeof name)
+
+var n1 = 10
+var n2 = 20
+
+n1 = '1' + 5
+
+console.log(n1 + n2)
