a23c9a52a68842c7a36c79ac604872c1cab4c30e
Tiago Pereira da Cruz <tiago.cruz@prodeb.local>
2019-07-10 17:26:59 -0300

    refatorando

--- cucumber/enjoeat/features/step_definitions/restaurantes_steps.rb
+++ cucumber/enjoeat/features/step_definitions/restaurantes_steps.rb
@@ -1,20 +1,20 @@
 # abaixo os códigos para o cenario data table
 
 Dado("que temos os seguintes restaurantes") do |table|
-    @restaurant_data = table.hashes
+  @restaurant_data = table.hashes
 end
 
 Quando("acesso a lista de restaurantes") do
-    @rest_list_page.load
+  @rest_list_page.load
 end
 
 Então("devo ver todos os restaurantes desta lista") do
-    restaurants = @rest_list_page.list
+  restaurants = @rest_list_page.list
 
-    @restaurant_data.each_with_index do |value, index|
-        expect(restaurants[index]).to have_text value['nome'].upcase
-        expect(restaurants[index]).to have_text value['categoria']
-        expect(restaurants[index]).to have_text value['entrega']
-        expect(restaurants[index]).to have_text value['avaliacao']
-    end
+  @restaurant_data.each_with_index do |value, index|
+    expect(restaurants[index]).to have_text value["nome"].upcase
+    expect(restaurants[index]).to have_text value["categoria"]
+    expect(restaurants[index]).to have_text value["entrega"]
+    expect(restaurants[index]).to have_text value["avaliacao"]
+  end
 end
